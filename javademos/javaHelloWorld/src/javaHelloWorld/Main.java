d03ebb4f2372f4219bf67ba0ba95a1bc4a8e2a4c
furkyto87 <35343468+furkyto87@users.noreply.github.com>
2022-09-21 02:15:07 +0300

    Add files via upload

---
+++ javademos/javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,12 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2");
+	}
+
+}
